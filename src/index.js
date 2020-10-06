88507b088c0e730d3bfd018a84d56409b502e4e3
dameyerdave <davmeyer@nexus-mac-11.home>
2020-10-06 13:15:09 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { DeviceEventEmitter } from 'react-native'
 import SyncStorage from 'sync-storage';
 
@@ -7,20 +7,19 @@ const initPersist = async () => {
         return await SyncStorage.init();
     }
     catch (err) {
-        console.log(err)
+        console.error(err)
     }
 }
 
 const usePersist = (_key, _default) => {
     const KEY = '@_@' + _key
-    const [obj, setObj] = useState(_default)
+    const [obj, setObj] = useState((SyncStorage.get(KEY) || _default))
 
     const set = (newObj) => {
         try {
             newObj = typeof newObj === 'function' ? newObj(obj) : newObj
-            console.log('set', newObj)
             setObj(newObj)
-            SyncStorage.set(KEY, JSON.stringify(newObj));
+            SyncStorage.set(KEY, newObj);
             DeviceEventEmitter.emit(KEY, newObj)
         }
         catch (err) {
@@ -29,14 +28,6 @@ const usePersist = (_key, _default) => {
     }
 
     useEffect(() => {
-        const value = JSON.parse(SyncStorage.get(KEY))
-        console.log('value', value)
-        if (value) {
-            setObj(value)
-        } else {
-            set(_default)
-        }
-
         const subscription = DeviceEventEmitter.addListener(KEY, (newObj) => { setObj(newObj) })
         return () => {
             subscription.remove()
