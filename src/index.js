6a51c43458b29a7215b5f82b7a71dbfb58170199
dameyerdave <dameyerdave@gmail.com>
2020-12-22 15:22:20 +0100

    fixed initialization of value

--- src/index.js
+++ src/index.js
@@ -3,45 +3,49 @@ import { DeviceEventEmitter } from 'react-native'
 import SyncStorage from 'sync-storage';
 
 const initPersist = async () => {
-    try {
-        return await SyncStorage.init();
-    }
-    catch (err) {
-        console.error(err)
-    }
+  try {
+    return await SyncStorage.init();
+  }
+  catch (err) {
+    console.error(err)
+  }
 }
 
 const usePersist = (_key, _default) => {
-    const KEY = '@_@' + _key
-    const [_, bump] = useState(0)
+  const KEY = '@_@' + _key
+  const [_, bump] = useState(0)
 
-    const set = (newObj) => {
-        // console.log('set', newObj, typeof newObj)
-        try {
-            newObj = typeof newObj === 'function' ? newObj(get()) : newObj
-            SyncStorage.set(KEY, newObj)
-            bump(current => current + 1)
-            DeviceEventEmitter.emit(KEY, newObj)
-        }
-        catch (err) {
-            console.error(err)
-        }
+  if (!SyncStorage.get(KEY)) {
+    SyncStorage.set(KEY, _default)
+  }
+
+  const set = (newObj) => {
+    // console.log('set', newObj, typeof newObj)
+    try {
+      newObj = typeof newObj === 'function' ? newObj(get()) : newObj
+      SyncStorage.set(KEY, newObj)
+      bump(current => current + 1)
+      DeviceEventEmitter.emit(KEY, newObj)
     }
-
-    const get = () => {
-        return SyncStorage.get(KEY)
+    catch (err) {
+      console.error(err)
     }
+  }
 
-    useEffect(() => {
-        const subscription = DeviceEventEmitter.addListener(KEY, () => {
-            bump(current => current + 1)
-        })
-        return () => {
-            subscription.remove()
-        }
-    }, [])
+  const get = () => {
+    return SyncStorage.get(KEY)
+  }
 
-    return [get, set]
+  useEffect(() => {
+    const subscription = DeviceEventEmitter.addListener(KEY, () => {
+      bump(current => current + 1)
+    })
+    return () => {
+      subscription.remove()
+    }
+  }, [])
+
+  return [get, set]
 }
 
 export { usePersist, initPersist }
